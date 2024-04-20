94e04882f1a362f93e7e61cd7ae9f8245c762d07
Sujal20031218 <sujalsinha127@gmail.com>
2024-04-20 15:00:07 +0530

    commiot testing

--- test.py
+++ test.py
@@ -60,7 +60,7 @@ assign a percentage match based on key criteria, and pinpoint missing keywords a
 resume:{text}
 description:{job_description}
 I want the response in one single string having the structure
-{{"Job Description Match":"%", "Missing Keywords":""}}
+"Job Description Match":"%", "Missing Keywords":""
 """
 
 # Streamlit app
@@ -70,37 +70,68 @@ with open('./style.css') as f:
 
 st.markdown(f'<style>{css}</style>', unsafe_allow_html=True)
 
-st.title("Upload Resume 🚀")
+st.title("Get Your Resume Score 🚀")
 st.markdown('<style>h1{color: black; text-align: center;}</style>', unsafe_allow_html=True)
+
 job_description = st.text_area("Paste the Job Description", height=300)
 uploaded_files = st.file_uploader("Upload Your Resume", type=["pdf", "docx"], accept_multiple_files=True, help="Please upload a single PDF or DOCX file")
-
 submit_button = st.button("Submit")
 
 if uploaded_files and len(uploaded_files) > 1:
     st.error("Please upload only one file at a time.")
 elif submit_button and uploaded_files:
-    uploaded_file = uploaded_files[0]
-    if uploaded_file.type == "application/pdf":
-        resume_text = extract_text_from_pdf_file(uploaded_file)
-    elif uploaded_file.type == "application/vnd.openxmlformats-officedocument.wordprocessingml.document":
-        resume_text = extract_text_from_docx_file(uploaded_file)
-    response_text = generate_response_from_gemini(input_prompt_template.format(text=resume_text, job_description=job_description))
-
-    # Extract Job Description Match percentage from the response
-    match_percentage_str = response_text.split('"Job Description Match":"')[1].split('"')[0]
-
-    # Remove percentage symbol and convert to float
-    match_percentage = float(match_percentage_str.rstrip('%'))
-
-    # Display ATS Evaluation Result
-    st.subheader("ATS Evaluation Result:")
-    st.write("```json")
-    st.write(response_text)
-    st.write("```")
-
-    # Highlight the Job Description Match percentage and display message
-    if match_percentage >= 70:
-        st.success(f" Match {match_percentage_str} - This resume matches the job description!")  # Highlight in green for a good match
+    if not job_description.strip():
+        st.error("Please provide the Job Description ⚠️")
     else:
-        st.error(f" Match {match_percentage_str} - This resume does not match the job description.")  # Highlight in red for a poor match
+        uploaded_file = uploaded_files[0]
+        if uploaded_file.type == "application/pdf":
+            resume_text = extract_text_from_pdf_file(uploaded_file)
+        elif uploaded_file.type == "application/vnd.openxmlformats-officedocument.wordprocessingml.document":
+            resume_text = extract_text_from_docx_file(uploaded_file)
+        
+        response_text = generate_response_from_gemini(input_prompt_template.format(text=resume_text, job_description=job_description))
+
+        # Extract Job Description Match percentage from the response
+        match_percentage_str = response_text.split('"Job Description Match":"')[1].split('"')[0]
+
+        if match_percentage_str == 'N/A':
+            st.error("Sorry, your skills do not match the requirements 😣")
+            
+            # Extract missing keywords from the response
+            missing_keywords = response_text.split('"Missing Keywords":"')[1].split('"')[0]
+            st.subheader("Missing Keywords:")
+            st.write(missing_keywords)
+            # Provide suggestions for improving the resume
+            st.subheader("How to Improve Your Resume:")
+            st.write("1. Incorporate the missing keywords from the job description into your resume.")
+            st.write("2. Tailor your resume to highlight relevant skills, experience, and accomplishments.")
+            st.write("3. Ensure your resume is clear, concise, and well-organized.")
+            st.write("4. Review and proofread your resume to eliminate any errors.")
+        else:
+            # Remove percentage symbol and convert to float
+            match_percentage = float(match_percentage_str.rstrip('%'))
+
+            # Display ATS Evaluation Result
+            st.subheader("ATS Evaluation Result:")
+            st.write("```json")
+            st.write(response_text)
+            st.write("```")
+            # Create a progress bar to represent the match percentage
+            # Show progress bar with a numerical value
+            st.write(f"Your Resume Match: {match_percentage_str}")
+            st.progress(match_percentage / 100)
+            # Highlight the Job Description Match percentage and display message
+            if match_percentage >= 70:
+                st.success(f" Your Resume Match is {match_percentage_str}😊 - This resume matches the job description!")  # Highlight in green for a good match
+            else:
+                st.error(f" Match {match_percentage_str}😭 - This resume does not match the job description.")  # Highlight in red for a poor match
+                
+                # Extract missing keywords from the response
+                missing_keywords = response_text.split('"Missing Keywords":"')[1].split('"')[0]
+                # Provide suggestions for improving the resume
+                st.subheader("How to Improve Your Resume:")
+                st.write("1. Incorporate the missing keywords from the job description into your resume.")
+                st.write("2. Tailor your resume to better highlight your skills and experience relevant to the job.")
+                # Recommend incorporating missing skills
+                st.write("3. **Add these skills to improve your resume:**")
+                st.write(missing_keywords)
